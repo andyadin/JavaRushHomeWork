092d14fc2cf411df3272bf52fcdebeea2b1837c4
andyadin <lynxvoodoo@yandex.ru>
2016-04-04 21:38:07 +0300

    First

--- src/com/javarush/test/level03/lesson06/task02/Solution.java
+++ src/com/javarush/test/level03/lesson06/task02/Solution.java
@@ -12,9 +12,9 @@ public class Solution
 {
     public static void main(String[] args)
     {
-        for (int i = 1; i < 10; i = i + 1)
+        for (int i = 1; i < 11; i = i + 1)
         {
-            for (int j = 1; j < 10; j = j + 1)
+            for (int j = 1; j < 11; j = j + 1)
             {
                 System.out.print(i * j + " ");
             }
